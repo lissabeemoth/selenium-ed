45cc0112661b6a8407eaf890950a0c03dbf60cf5
lissa <lissa.beemoth@gmail.com>
2017-07-18 23:36:44 +0300

    Replaced Geckodriver to Chromedriver

--- first.py
+++ first.py
@@ -1,12 +1,17 @@
+# -*- coding: UTF-8 -*-
 import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+import sys
+print sys.getdefaultencoding()
+
 
 @pytest.fixture
 def driver(request):
-    wd = webdriver.Firefox(firefox_binary="c:\\Program Files (x86)\\Nightly\\firefox.exe")
+    # wd = webdriver.Firefox(firefox_binary="/Applications/Firefox.app")
+    wd = webdriver.Chrome()
     print(wd.capabilities)
     request.addfinalizer(wd.quit)
     return wd
@@ -16,4 +21,4 @@ def test_example(driver):
     driver.get("http://www.google.com/")
     driver.find_element_by_name("q").send_keys("webdriver")
     driver.find_element_by_name("btnG").click()
-    WebDriverWait(driver, 10).until(EC.title_is("webdriver - Поиск в Google"))
+    WebDriverWait(driver, 10).until(EC.title_is("Google"))
