e1a3e3aa2eb819bc0905e972b0a18b3344e7c280
lissa <lissa.beemoth@gmail.com>
2017-08-04 12:57:44 +0300

    task 5.11

--- 5_11.py
+++ 5_11.py
@@ -3,7 +3,6 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import random
 
